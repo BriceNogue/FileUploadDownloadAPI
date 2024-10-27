b3ce33446030ee9892c98c0e042f2bd72878e711
BriceNogue <bricenogue@gmail.com>
2024-10-27 12:42:53 +0100

    Ajoutez des fichiers projet.

---
+++ Infrastructure/Context/DataContext.cs
@@ -0,0 +1,23 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Context
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=localhost;Database=FilesDB;Trusted_Connection=true;TrustServerCertificate=true;")
+                .UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+        }
+
+        public DbSet<FileEntity> Files { get; set; }
+    }
+}
